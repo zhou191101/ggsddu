a9b191aaabc15fc8baf8e452a2b66db5f898cb57
dorayo <yomingyo@gmail.com>
2019-08-05 15:08:40 +0800

    spark udf

---
+++ algorithms/src/main/java/com/ggsddu/PowerOfTwo.java
@@ -0,0 +1,11 @@
+package com.ggsddu;
+
+/**
+ * @author zhoup
+ */
+public class PowTow {
+	public static void main(String[] args){
+
+		System.out.println(4 & 1 );
+	}
+}
---
+++ spark/src/main/scala/com/ggsddu/udf/PPXUDF.scala
@@ -0,0 +1,41 @@
+package com.ggsddu.udf
+
+import org.apache.spark.sql.api.java.UDF1
+
+class PPXUDF extends UDF1[List[(Double, Int)], (Double, Double)] {
+
+  override def call(t1: List[(Double, Int)]): (Double, Double) = {
+
+    if (t1 == null || t1.isEmpty) {
+      (0.825234800758695, 0.5)
+    } else {
+      var appScore = t1.groupBy(_._2).mapValues(_.map(_._1).sum)
+
+      if (!appScore.keySet.contains(1)) {
+        appScore += (1 -> 0)
+
+      } else if (!appScore.keySet.contains(2)) {
+        appScore += (2 -> 0)
+      }
+
+      val result = appScore.map(map => {
+        val key = map._1
+        val value = map._2
+        key match {
+          case 1 => (key, 1 / (1 + Math.exp(value + -1.55222459843)))
+          case _ => (key, 1 / (1 + Math.exp(value + 0)))
+        }
+      }).toSeq.sortBy(_._1)
+      (result(0)._2, result(1)._2)
+    }
+
+  }
+}
+
+object PPXUDF {
+  def main(args: Array[String]): Unit = {
+    val test = new PPXUDF
+    val res = test.call(List((-0.12, 1), (-0.13, 2), (0.123, 2)))
+    println(res)
+  }
+}
