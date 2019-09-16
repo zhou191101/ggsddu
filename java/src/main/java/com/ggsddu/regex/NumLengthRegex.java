1ab38cb303388ac1fcf472d283db0efd19bb11af
dorayo <yomingyo@gmail.com>
2019-09-16 11:10:06 +0800

    regex test

--- java/src/main/java/com/ggsddu/regex/NumLengthRegex.java
+++ java/src/main/java/com/ggsddu/regex/NumLengthRegex.java
@@ -7,15 +7,22 @@ import java.util.regex.Pattern;
  */
 public class NumLengthRegex {
 
-	private static final Pattern pattern = Pattern.compile("\\d{11}|\\d{17}");
+	private static final Pattern pattern = Pattern
+			.compile("^000860[1][3-9][0-9]{9}$|^0000010[0-9]{10}$|^00088600[0-9]{9}|^000852000([6|9])[0-9]{7}|^00085300000([6|8])[0-9]{5}");
 
 	public static void main(String[] args){
 
-		String num1 = "18081587866";
-		String num2 = "18080187867123456";
+		String num1 = "00086018080587865";
+		String num2 = "00000105613662033";
+		String num3 = "00088600935921609";
+		String num4 = "00085200098623039";
+		String num5 = "00085300000824372";
 
 		System.out.println(pattern.matcher(num1).matches());
 		System.out.println(pattern.matcher(num2).matches());
+		System.out.println(pattern.matcher(num3).matches());
+		System.out.println(pattern.matcher(num4).matches());
+		System.out.println(pattern.matcher(num5).matches());
 
 
 	}
