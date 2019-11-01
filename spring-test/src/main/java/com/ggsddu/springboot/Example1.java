05075463aad58a342589e62bda9a5f8808458248
zhoupeng <abc1095014900@qq.com>
2019-11-01 10:25:49 +0800

    spring boot hello world

--- spring-test/src/main/java/com/ggsddu/springboot/Example1.java
+++ spring-test/src/main/java/com/ggsddu/springboot/Example1.java
@@ -1,20 +1,15 @@
 package com.ggsddu.springboot;
 
-
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @EnableAutoConfiguration
-public class Example {
-    @RequestMapping("/home")
-    String home() {
-        return "Hello World!";
-    }
+public class Example1 {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Example.class, args);
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello World";
     }
 }
