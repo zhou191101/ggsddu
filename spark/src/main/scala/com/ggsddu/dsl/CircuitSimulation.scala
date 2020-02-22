f6053942bddea5b861b269c996c73fff35d20fd9
zhoupeng <abc1095014900@qq.com>
2020-02-22 14:15:34 +0800

    add dsl

---
+++ spark/src/main/scala/com/ggsddu/dsl/CircuitSimulation.scala
@@ -0,0 +1,19 @@
+package com.ggsddu.dsl
+
+abstract class CircuitSimulation extends BasicCircuitSimulation {
+  def halfAdder(a: Wire, b: Wire, s: Wire, c: Wire) = {
+    val d, e = new Wire
+    orGate(a, b, d)
+    andGate(a, b, c)
+    inverter(c, e)
+    andGate(d, e, s)
+  }
+
+  def fullAdder(a: Wire, b: Wire, cin: Wire, sum: Wire, count: Wire) = {
+    val s, c1, c2 = new Wire
+    halfAdder(a, cin, s, c1)
+    halfAdder(b, s, sum, c2)
+    orGate(c1, c2, count)
+  }
+
+}
