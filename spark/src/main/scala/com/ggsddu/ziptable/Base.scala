b23b3acc8ff653f65139e180ef25fb39b911c692
zhoupeng <abc1095014900@qq.com>
2020-03-02 18:22:44 +0800

    add spark util

--- spark/src/main/scala/com/ggsddu/ziptable/Base.scala
+++ spark/src/main/scala/com/ggsddu/ziptable/Base.scala
@@ -1,11 +1,42 @@
 package com.ggsddu.ziptable
 
 import org.apache.spark.SparkConf
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, SparkSession}
 
 trait Base {
-  val conf = new SparkConf()
-  lazy val spark = SparkSession.builder().config(conf).getOrCreate()
 
-  def execute(): Unit
+  def conf(): SparkConf
+
+  lazy val spark: SparkSession = SparkSession.builder().config(conf()).getOrCreate()
+
+  def execute(args: Array[String]): Unit
+
+  def writeToZipTable(fullDF: DataFrame, updateDF: DataFrame, primaryKeys: Set[String],
+                      endDay: String, f: DataFrame => Unit): Unit = {
+
+    val updateFields = updateDF.schema.fields.map(_.name).toList
+    val fullFields = fullDF.schema.fields.map(_.name).toList
+
+    updateDF.createOrReplaceTempView("tmp_update")
+
+    fullDF.createOrReplaceTempView("tmp_full")
+
+    val updateSql =
+      s"""
+         |select ${fullFields.filter(!_.equals("end_date")).map(field => "tmp_full." + field).mkString(",")},
+         |    case when ${primaryKeys.map(key => "update_zip." + key + " is not null").mkString(" and ")}
+         |    then "${endDay}"
+         |    else tmp_full.end_date
+         |    end as end_date
+         |from tmp_full
+         |left join
+         |(select ${primaryKeys.mkString(",")} from tmp_update) update_zip
+         |on ${primaryKeys.map(key => "tmp_full." + key + "=" + "update_zip." + key).mkString(" and ")}
+         |union all
+         |select ${updateFields.mkString(",")}, "${endDay}" as start_date,"99999999" as end_date
+         |from tmp_update
+         |""".stripMargin
+    val result = spark.sql(updateSql)
+    f(result)
+  }
 }
--- spark/src/main/scala/com/ggsddu/ziptable/ZipTable.scala
+++ spark/src/main/scala/com/ggsddu/ziptable/ZipTable.scala
@@ -1,20 +1,36 @@
 package com.ggsddu.ziptable
 
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.{Column, DataFrame}
+
 object ZipTable extends Base {
 
 
-  override def execute(): Unit = {
-    val df = spark.read.format("csv")
-      .option("header", true)
-      .option("delimiter","\t")
-      .load("/Users/zhoupeng/Desktop/spark-zip-test.csv")
-    df.show()
+  override def execute(args: Array[String]): Unit = {
+    val updateDf = readFromCSV("/Users/zhoupeng/Desktop/hott-20200302", header = true, "\t")
+    val fullDf = readFromCSV("/Users/zhoupeng/Desktop/full-hott", header = true, ",")
+    val endDay = "20200302"
+    writeToZipTable(fullDf, updateDf, Set("combine", "sold_to"), endDay,
+      df => df.write.mode("overwrite")
+        .format("csv").option("header", value = true)
+        .save("/Users/zhoupeng/Desktop/full-hott2")
+    )
 
   }
 
   def main(args: Array[String]): Unit = {
-    conf.setAppName("zip_table")
-    conf.setMaster("local[*]")
-    execute()
+    execute(args)
+  }
+
+  def readFromCSV(path: String, header: Boolean, sep: String): DataFrame = {
+    spark.read.format("csv")
+      .option("header", value = header)
+      .option("delimiter", sep)
+      .load(path)
+  }
+
+
+  override def conf(): SparkConf = {
+    new SparkConf().setAppName("zip_table").setMaster("local[1]")
   }
 }
