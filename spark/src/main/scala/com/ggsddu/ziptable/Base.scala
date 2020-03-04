07452779c1d8bf65ca64707dfd37efa92663b4ab
zhoupeng <abc1095014900@qq.com>
2020-03-04 13:17:19 +0800

    remove rich method

--- spark/src/main/scala/com/ggsddu/ziptable/Base.scala
+++ spark/src/main/scala/com/ggsddu/ziptable/Base.scala
@@ -46,40 +46,4 @@ trait Base {
     val result = spark.sql(updateSql)
     write(result)
   }
-
-  def writeToZipTable(fullDF: DataFrame, updateDF: DataFrame, primaryKeys: Set[String],
-                      endDay: String, write: DataFrame => Unit): Unit = {
-
-    val fullFields = fullDF.schema.fields.map(_.name).toList
-
-    updateDF.createOrReplaceTempView("tmp_update")
-
-    fullDF.createOrReplaceTempView("tmp_full")
-
-    val updateSql =
-      s"""
-         |select ${fullFields.filter(!_.equals("end_date")).map(field => "tmp_full_update." + field).mkString(",")},
-         |    case when ${primaryKeys.map(key => "update_zip." + key + " is not null").mkString(" and ")}
-         |    then "${endDay}"
-         |    else tmp_full_update.end_date
-         |    end as end_date
-         |from
-         |(select * from tmp_full where end_date='99999999')tmp_full_update
-         |left join
-         |(select ${primaryKeys.mkString(",")} from tmp_update) update_zip
-         |on ${primaryKeys.map(key => "tmp_full_update." + key + "=" + "update_zip." + key).mkString(" and ")}
-         |
-         |union all
-         |
-         |select ${fullFields.mkString(",")} from tmp_full where end_date !='99999999'
-         |
-         |union all
-         |
-         |select ${fullFields.filter(x => !Set("start_date", "end_date").contains(x)).mkString(",")},
-         |    "${endDay}" as start_date,"99999999" as end_date
-         |from tmp_update
-         |""".stripMargin
-    val result = spark.sql(updateSql)
-    write(result)
-  }
 }
