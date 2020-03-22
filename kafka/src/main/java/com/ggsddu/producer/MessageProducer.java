ee38ccaa9334b3b6582a2232a0e5e1ecf1eecdc4
zhoupeng <abc1095014900@qq.com>
2020-03-22 14:54:42 +0800

    kafka producer

--- kafka/src/main/java/com/ggsddu/producer/MessageProducer.java
+++ kafka/src/main/java/com/ggsddu/producer/MessageProducer.java
@@ -10,42 +10,42 @@ import java.util.Properties;
  */
 public class MessageProducer {
 
-	private static final String TOPIC = "test1";
-	private static final String BROKER_LIST = "localhost:9092";
-	private static KafkaProducer<String, String> producer = null;
+    private static final String TOPIC = "test";
+    private static final String BROKER_LIST = "localhost:9092";
+    private static KafkaProducer<String, String> producer;
 
-	static{
-		Properties configs = initConfig();
-		producer = new KafkaProducer<>(configs);
-	}
+    static {
+        Properties configs = initConfig();
+        producer = new KafkaProducer<>(configs);
+    }
 
-	private static Properties initConfig(){
-		Properties properties = new Properties();
-		properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BROKER_LIST);
-		properties.put(ProducerConfig.ACKS_CONFIG, "all");
-		properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-		properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-		return properties;
-	}
+    private static Properties initConfig() {
+        Properties properties = new Properties();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BROKER_LIST);
+        properties.put(ProducerConfig.ACKS_CONFIG, "all");
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        return properties;
+    }
 
-	public static void main(String[] args){
+    public static void main(String[] args) {
 
-		try{
-			String message = "hello world";
-			ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC, message);
-			producer.send(record, (recordMetadata, e) -> {
-				if(null == e){
-					System.out.println("perfect!");
-				}
-				if(null != recordMetadata){
-					System.out.println(
-							"offset: " + recordMetadata.offset() + ";partition: " + recordMetadata.partition());
-				}
-			}).get();
-		} catch(Exception e){
-			e.printStackTrace();
-		} finally{
-			producer.close();
-		}
-	}
+        try {
+            String message = "hello world";
+            ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC, message);
+            producer.send(record, (recordMetadata, e) -> {
+                if (null == e) {
+                    System.out.println("perfect!");
+                }
+                if (null != recordMetadata) {
+                    System.out.println(
+                            "offset: " + recordMetadata.offset() + "; partition: " + recordMetadata.partition());
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            producer.close();
+        }
+    }
 }
