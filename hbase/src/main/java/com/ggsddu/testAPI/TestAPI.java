5fd607b6eb2081fb705db2a97cae9536deed83d1
zhoupeng <abc1095014900@qq.com>
2020-07-24 10:24:08 +0800

    add hbase model

---
+++ hbase/src/main/java/com/ggsddu/testAPI/TestAPI.java
@@ -0,0 +1,20 @@
+package com.ggsddu.testAPI;
+
+/**
+ * DDL
+ * 1.判断表是否存在
+ * 2.创建表
+ * 3.创建namespace
+ * 4.删除表
+ * DML
+ * 1.插入数据
+ * 2.查询数据
+ *   2.1 get
+ *   2.2 scan
+ * 3.删除数据
+ */
+public class TestAPI {
+    public static void main(String[] args) {
+
+    }
+}
