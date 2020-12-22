cef819fe5e98fd8e8c406ff79a95f00c2d29cda6
zhoupeng <abc1095014900@qq.com>
2020-12-22 14:25:58 +0800

    binary test

---
+++ java/src/main/java/com/ggsddu/binary/Test.java
@@ -0,0 +1,11 @@
+package com.ggsddu.binary;
+
+import java.util.Arrays;
+
+public class Test {
+    public static void main(String[] args) {
+
+        System.out.println(Arrays.toString("asa".getBytes()));
+        System.out.println(Integer.toBinaryString(100));
+    }
+}
